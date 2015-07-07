0ace1ad348265ec3479c07472a64bf38456df081
Jed Liu <liujed@cs.cornell.edu>
2015-07-07 18:14:50 -0400

    Removed extraneous import.

--- src/jif/ide/editors/JifEditor.java
+++ src/jif/ide/editors/JifEditor.java
@@ -3,7 +3,6 @@ package jif.ide.editors;
 import jif.ide.JifPluginInfo;
 import polyglot.ide.PluginInfo;
 import polyglot.ide.editors.JLEditor;
-import polyglot.ide.editors.SourceViewerConfiguration;
 
 public class JifEditor extends JLEditor {
 
